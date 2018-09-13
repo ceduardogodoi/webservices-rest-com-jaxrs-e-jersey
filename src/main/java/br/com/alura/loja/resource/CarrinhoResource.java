fb7f39b104e806d69e222eaab32e06eb958d955d
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-09-13 06:30:54 -0300

    Aula 13 Atividade 01: Deploy WAR e Servlet Contêiners

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -30,7 +30,7 @@ public class CarrinhoResource {
 //		
 //		return carrinho.toJson();
 //	}
-	
+
 	@Path("{id}")
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
@@ -39,36 +39,36 @@ public class CarrinhoResource {
 
 		return carrinho.toXml();
 	}
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_XML)
 	public Response adiciona(String conteudo) {
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
-		
+
 		new CarrinhoDAO().adiciona(carrinho);
-		
+
 		URI uri = URI.create("/carrinhos/" + carrinho.getId());
-		
+
 		return Response.created(uri).build();
 	}
-	
+
 	@Path("{id}/produtos/{produtoId}")
 	@DELETE
 	public Response removeProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		carrinho.remove(produtoId);
-		
+
 		return Response.ok().build();
 	}
-	
+
 	@Path("{id}/produtos/{produtoId}/quantidade")
 	@PUT
 	public Response alteraProduto(String conteudo, @PathParam("id") long id, @PathParam("produtoId") long produtoId) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
-		
+
 		Produto produto = (Produto) new XStream().fromXML(conteudo);
 		carrinho.trocaQuantidade(produto);
-		
+
 		return Response.ok().build();
 	}
 
